4015f5b0b74755630a85f4708f0ad03d4ddcf743
efenatuyo <113731512+efenatuyo@users.noreply.github.com>
2022-09-21 12:10:57 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -4,7 +4,7 @@ Title Installing modules
 Echo =====================
 Echo    Module installer
 Echo =====================
-Echo Press any key to start Basic Scan
+Echo Press any key to start Basic installer
 Pause>nul
 Echo started the module installer...
 pip install -r requirements.txt
