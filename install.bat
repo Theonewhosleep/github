7bcdfeee1edfcb8729c7790679106599b366fc7a
efenatuyo <113731512+efenatuyo@users.noreply.github.com>
2022-09-21 12:09:10 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,1 +1,13 @@
-
+Color 02
+Title Installing modules
+Echo =====================
+Echo    Module installer
+Echo =====================
+Echo Press any key to start Basic Scan
+Pause>nul
+Echo started the module installer...
+pip install -r requirements.txt
+Echo done
+Echo Press any key to exit
+Pause>nul
+Exit
